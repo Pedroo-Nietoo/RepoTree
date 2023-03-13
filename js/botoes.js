e9d487631163a5a95dfabd1a3a2aa3551fb76385
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-03-13 14:21:30 -0300

    RepoTree

--- js/botoes.js
+++ js/botoes.js
@@ -1,33 +1,9 @@
-var itemLinkedIn = document.querySelector(".itemLinkedIn");
-var itemGitHub = document.querySelector(".itemGitHub");
-var itemInstagram = document.querySelector(".itemInstagram");
-
-itemLinkedIn.addEventListener("mouseover", function () {
-    itemGitHub.style.backgroundColor = "#777";
-    itemInstagram.style.backgroundColor = "#777";
-});
-
-itemGitHub.addEventListener("mouseover", function () {
-    itemLinkedIn.style.backgroundColor = "#777";
-    itemInstagram.style.backgroundColor = "#777";
-});
-
-itemInstagram.addEventListener("mouseover", function () {
-    itemLinkedIn.style.backgroundColor = "#777";
-    itemGitHub.style.backgroundColor = "#777";
-});
-
-itemLinkedIn.addEventListener("mouseout", function () {
-    itemGitHub.style.backgroundColor = "#fff";
-    itemInstagram.style.backgroundColor = "#fff";
-});
-
-itemGitHub.addEventListener("mouseout", function () {
-    itemLinkedIn.style.backgroundColor = "#fff";
-    itemInstagram.style.backgroundColor = "#fff";
-});
-
-itemInstagram.addEventListener("mouseout", function () {
-    itemLinkedIn.style.backgroundColor = "#fff";
-    itemGitHub.style.backgroundColor = "#fff";
-});
+function abrirLinkedIn() {
+    window.open("https://www.linkedin.com/in/pedroo-nietoo/")
+}
+function abrirGitHub() {
+    window.open("https://github.com/Pedroo-Nietoo")
+}
+function abrirInstagram() {
+    window.open("https://www.instagram.com/pedroo_nietoo/")
+}
