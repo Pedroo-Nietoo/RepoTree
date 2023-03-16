f0763fc4816bd29ef846a55eef28e1c5694b7a63
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-03-16 12:20:31 -0300

    Add Newsletter

--- js/botoes.js
+++ js/botoes.js
@@ -1,9 +1,13 @@
 function abrirLinkedIn() {
-    window.open("https://www.linkedin.com/in/pedroo-nietoo/")
+  window.open("https://www.linkedin.com/in/pedroo-nietoo/");
 }
 function abrirGitHub() {
-    window.open("https://github.com/Pedroo-Nietoo")
+  window.open("https://github.com/Pedroo-Nietoo");
 }
 function abrirInstagram() {
-    window.open("https://www.instagram.com/pedroo_nietoo/")
+  window.open("https://www.instagram.com/pedroo_nietoo/");
+}
+
+function assinarNewsletter() {
+  window.open("https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7036051610770255872");
 }
